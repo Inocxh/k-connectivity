9da44304fb5f67d7a15bd37b5360de160a0d08ca
Inocxh <s204453@student.dtu.dk>
2022-02-16 12:26:27 +0100

    wowow

---
+++ src/main/java/Graph.java
@@ -0,0 +1,10 @@
+public interface Graph {
+
+    /// Returns the number of vertices in the graph
+    int getN();
+    /// Returns the number of edges in the graph
+    int getM();
+
+    /// Returns the list of vertices neighbouring v
+    int[] getNeighbours(int v);
+}
