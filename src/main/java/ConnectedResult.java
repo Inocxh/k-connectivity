39c5ffe698ce69a41e4b8af17ed5caf8fa4b3c96
Inocxh <s204453@student.dtu.dk>
2022-02-16 12:43:32 +0100

    wowow

---
+++ src/main/java/ConnectedResult.java
@@ -0,0 +1,11 @@
+public enum ConnectedResult {
+    None,
+    NotConnected,
+    HasBrigde,
+    TwoEdgeConnected,
+    TwoConnected,
+    ThreeEdgeConnected,
+    ThreeConnected,
+    FourEdgeConnected,
+    FourConnected
+}
