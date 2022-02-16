1f86fc3e75fe5fb6d40e401c11665eaaad71b903
AndersGadLauridsen <agl@aety.io>
2022-02-16 13:58:44 +0100

    temp

--- src/main/java/DFSTree.java
+++ src/main/java/DFSTree.java
@@ -1,3 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.*;
+
 public class DFSTree {
+    public DFSTree(Graph G, int root) {
+       int i = 0 ;
+       HashSet visited = new HashSet();
+       Stack<Integer> stack = new Stack<Integer>();
+       stack.push(root);
+       while (!stack.isEmpty()) {
+           int currentNode = stack.pop();
+           for(int neighbour: G.getNeighbours(currentNode) {
+              stack.push(neighbour);
+
+
+           }
+
+       }
+       for currentNode.children
+
+    }
+    public int[] getChildren(int node) {
+        return children;
+    }
+    public int getParent(int node) {
+        return parent;
+    }
+    public int[] dfsOrder() {
+
+    }
 
 }
