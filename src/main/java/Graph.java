bfc8ba8d1bc23896dad6b60e52fa82c865bc786d
Inocxh <s204453@student.dtu.dk>
2022-03-09 14:42:59 +0100

    its broken :(

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,10 +1,12 @@
+import java.util.List;
+
 public interface Graph {
     /// Returns the number of vertices in the graph
     int getN();
     /// Returns the number of edges in the graph
     int getM();
     /// Returns the list of vertices neighbouring v
-    int[] getNeighbours(int v);
+    List<Integer> getNeighbours(int v);
     // Add an edge
     void addEdge(int v, int w);
 }
