ca4f1e4f2b85d628798fe9a6bc7fc55dfc0765d3
MagnusSiegumfeldt <m.ks@outlook.dk>
2022-03-23 15:57:35 +0100

    Refactor of ChainDecomp. This is now in ChainDecomposition class. Inherit from DFStree removed

--- src/main/java/Schmidt.java
+++ src/main/java/Schmidt.java
@@ -1,16 +1,17 @@
+
 public class Schmidt {
 
     public static ConnectedResult is2Connected(Graph G) {
-        ChainDFSTree T = new ChainDFSTree(G, 0);
-        T.printResults();
+        DFSTree T = new DFSTree(G, 0, false);
+        ChainDecomposition CD = new ChainDecomposition(T, false);
 
-        if (T.getCEdges() < G.getM()){
+        if (CD.getCEdges() < G.getM()){
             System.out.println("Not 2-edge-connected");
             return ConnectedResult.HasBridge;
-        } else if (T.getCCycles() > 1){
+        } else if (CD.getCCycles() > 1){
             System.out.println("2-edge-connected, not 2-connected");
             return ConnectedResult.TwoEdgeConnected;
-        } else if (T.getCCycles() == 1){
+        } else if (CD.getCCycles() == 1){
             return ConnectedResult.TwoConnected;
         } else { // Should never be reached
             return ConnectedResult.None;
