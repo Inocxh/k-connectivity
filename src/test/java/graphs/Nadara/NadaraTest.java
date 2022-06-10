4a334a22f49b181f88db540141af44a86b3cc9a1
s194587 <s194587@student.dtu.dk>
2022-06-10 11:33:46 +0200

    Changed assert statements in nadara test file

--- src/test/java/graphs/Nadara/NadaraTest.java
+++ src/test/java/graphs/Nadara/NadaraTest.java
@@ -15,40 +15,40 @@ public class NadaraTest {
     public void lowsMissing () throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/3para.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
 
     @Test
     public void lowerCase1() throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/lower.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
     @Test
     public void lowerCase2() throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/lower2.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
 
     @Test
     public void upperCase1() throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/upper.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
     @Test
     public void upperCase2() throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/upperNoMax.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
 
     @Test
     public void manyCuts1() throws Exception {
         File f = new File("src/test/graphs/correctness/nadara/minfuncs.gr");
         ArrayGraph g = ArrayGraph.fromString(new Scanner(f));
-        assert(Nadara.Nadara(g) == ConnectedResult.ThreeEdgeConnected);
+        assert(Nadara.is4EdgeConnected(g) == ConnectedResult.ThreeEdgeConnected);
     }
 
     @Test
